7b6c7657562dde38e35a8eaa662b9a61cc3b861a
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-11-17 17:52:02 +0000

    Refactor to show legends in graphs

--- data_studies/GraphicsGenerator.py
+++ data_studies/GraphicsGenerator.py
@@ -4,6 +4,15 @@ def create_comparison_graphic_with(input_y, predicted_y):
 
     if(len(input_y) != len(predicted_y)):
         print("Both dataframes should have the same amount of elements")
+        return
 
-    plt.plot(range(0, len(input_y)), input_y, predicted_y)
+    plt.plot(input_y)
+    plt.plot(predicted_y)
+    plt.legend(labels=['input_y', 'predicted_y'])
+    plt.show()
+
+
+def plot_loss(loss):
+    plt.plot(loss)
+    plt.legend(labels=['loss'])
     plt.show()
